c74335c1ab0bdd022d6ee6a97d8189a6b11326e6
Lalitha-Bandaru <bhavanilalitha7197@gmail.com>
2026-06-19 22:11:36 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/weather-app/",
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/weather-app/",
->>>>>>> a3eb7c939a76506e478cfebfa192146723b4f5ff
+  base: "/weather-app/"
 });
