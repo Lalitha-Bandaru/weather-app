82402d0638950c288ed3503855e6db8b32a20c8d
Lalitha-Bandaru <bhavanilalitha7197@gmail.com>
2026-06-19 21:55:12 +0530

    merge remote

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
->>>>>>> daa58368103d0328baa95c5623e308afb053bbb8
 export default defineConfig({
   plugins: [react()],
-  base: "/YOUR-REPO-NAME/"
+  base: "/weather-app/",
 });
